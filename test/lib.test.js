cf29480a837a16828aff460a460fa335e6b42c6b
Andreas Dzialocha <x12@adz.garden>
2020-10-22 19:11:38 +0200

    Update tests

--- test/lib.test.js
+++ test/lib.test.js
@@ -1,18 +1,21 @@
+import '@babel/polyfill';
 import findTransitiveTransfer from '../src';
 
-import graph1 from './graph-1.json';
-import graph2 from './graph-2.json';
-import graph3 from './graph-3.json';
-import graph4 from './graph-4.json';
-
 import { expectSuccessfulTransfer } from './utils';
+
+const PATHFINDER_EXECUTABLE = './pathfinder';
+
+const graph1 = './test/graph-1.json';
+const graph2 = './test/graph-2.json';
+const graph3 = './test/graph-3.json';
+const graph4 = './test/graph-4.json';
 
 const testVectorsSuccess = [
   {
     graph: graph1,
     transaction: {
-      from: 'A',
-      to: 'D',
+      from: '0x5534d2ba89ad1c01c186efafee7105dba071134a',
+      to: '0x29003579d2ca6d47c1860c4ed36656542a28f012',
       value: 10,
     },
     expected: {
@@ -27,8 +30,8 @@ const testVectorsSuccess = [
       edges: [],
     },
     transaction: {
-      from: 'A',
-      to: 'D',
+      from: '0x5534d2ba89ad1c01c186efafee7105dba071134a',
+      to: '0x29003579d2ca6d47c1860c4ed36656542a28f012',
       value: 10,
     },
     expected: {
@@ -40,8 +43,8 @@ const testVectorsSuccess = [
   {
     graph: graph1,
     transaction: {
-      from: 'A',
-      to: 'D',
+      from: '0x5534d2ba89ad1c01c186efafee7105dba071134a',
+      to: '0x29003579d2ca6d47c1860c4ed36656542a28f012',
       value: 100,
     },
     expected: {
@@ -53,8 +56,8 @@ const testVectorsSuccess = [
   {
     graph: graph2,
     transaction: {
-      from: '1',
-      to: '13',
+      from: '0x12e3db638ff9ac639425f24fad3193cb72b4e7fb',
+      to: '0xa559aa8ed21434ebfa23958bc27d201391929219',
       value: 50,
     },
     expected: {
@@ -66,8 +69,8 @@ const testVectorsSuccess = [
   {
     graph: graph2,
     transaction: {
-      from: '99',
-      to: '44',
+      from: '0xe4ec3ccfd5cdb641ec13305b6ef3536915a2688d',
+      to: '0xd9e13bb778b1d4dc87053f3912c597c64306a91e',
       value: 80,
     },
     expected: {
@@ -79,8 +82,8 @@ const testVectorsSuccess = [
   {
     graph: graph3,
     transaction: {
-      from: 'A',
-      to: 'B',
+      from: '0x2f764f3b669093dd24648757971070172ca2af33',
+      to: '0xa0ff2f1b0ab2414e571bcd134781d746c750916b',
       value: 50,
     },
     expected: {
@@ -92,8 +95,8 @@ const testVectorsSuccess = [
   {
     graph: graph4,
     transaction: {
-      from: '11',
-      to: '50',
+      from: '0xd615e7351261d1bd8558742015adffff15a425d7',
+      to: '0x7875dfd647efa680b83e418fc00b3e38b7442bc6',
       value: 50,
     },
     expected: {
@@ -209,22 +212,23 @@ const testVectorsFail = [
 const testVectors = [...testVectorsSuccess, ...testVectorsFail];
 
 describe('findTransitiveTransfer', () => {
-  it('should run test vectors successfully', () => {
-    testVectors.forEach((vector) => {
-      let result;
-
-      const test = () => {
-        result = findTransitiveTransfer({
-          ...vector.graph,
-          ...vector.transaction,
+  it('should run test vectors successfully', async () => {
+    for (const vector of testVectors) {
+      const test = async () => {
+        return await findTransitiveTransfer(vector.transaction, {
+          edgesFile: vector.graph,
+          pathfinderExecutable: PATHFINDER_EXECUTABLE,
         });
       };
 
       if (vector.expected.error) {
-        expect(test).toThrow(new Error(vector.expected.error));
-      } else {
-        expect(test).not.toThrow();
+        expect.assertions(1);
+        return test().catch((e) =>
+          expect(e).toMatch(new Error(vector.expected.error)),
+        );
+      }
 
+      return test().then((result) => {
         const { expected, transaction } = vector;
         expect(result.from).toBe(transaction.from);
         expect(result.to).toBe(transaction.to);
@@ -236,7 +240,7 @@ describe('findTransitiveTransfer', () => {
           ...result,
           ...vector.graph,
         });
-      }
-    });
+      });
+    }
   });
 });
